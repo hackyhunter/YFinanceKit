80f632b77647e53a2527139af3441ca4f01610ff
Johnny Hunter <88464667+hackyhunter@users.noreply.github.com>
2026-08-18 16:15:58 -0700

    Test Retry-After parsing

---
+++ Tests/YFinanceKitTests/YFinanceRetryAfterTests.swift
@@ -0,0 +1,28 @@
+import Foundation
+import XCTest
+@testable import YFinanceKit
+
+final class YFinanceRetryAfterTests: XCTestCase {
+    func testParsesDeltaSeconds() {
+        XCTAssertEqual(YFRetryAfterParser.parse("15"), 15)
+        XCTAssertEqual(YFRetryAfterParser.parse(" 2.5 "), 2.5)
+        XCTAssertNil(YFRetryAfterParser.parse("-1"))
+        XCTAssertNil(YFRetryAfterParser.parse("banana"))
+    }
+
+    func testCapsAbsurdDeltaSeconds() {
+        XCTAssertEqual(YFRetryAfterParser.parse("999999", maximum: 60), 60)
+    }
+
+    func testParsesHTTPDateRelativeToInjectedClock() {
+        let now = Date(timeIntervalSince1970: 1_784_403_200) // 2026-07-18 00:00:00 UTC
+        let raw = "Sat, 18 Jul 2026 00:00:30 GMT"
+        XCTAssertEqual(YFRetryAfterParser.parse(raw, now: now), 30)
+    }
+
+    func testPastHTTPDateReturnsZero() {
+        let now = Date(timeIntervalSince1970: 1_784_403_200)
+        let raw = "Fri, 17 Jul 2026 23:59:00 GMT"
+        XCTAssertEqual(YFRetryAfterParser.parse(raw, now: now), 0)
+    }
+}
